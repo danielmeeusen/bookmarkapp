bb633cef5d1ffad501a55df3a196e069ea3f6329
Daniel Meeusen <danmeeusen@gmail.com>
2018-05-14 22:46:52 -0700

    formatting & visit fix

--- js/main.js
+++ js/main.js
@@ -7,15 +7,21 @@ function saveBookmark(e) {
     var siteName = document.getElementById('siteName').value;
     var siteUrl = document.getElementById('siteUrl').value;
 
+    siteUrl = fixUrl(siteUrl);
+
+    console.log(validateForm(siteName, siteUrl));   
+
     if(!validateForm(siteName, siteUrl)){
         return false;
+        e.preventDefault();
+    } else {
+        fetchBookmarks();
     }
  
     var bookmark = {
         name: siteName,
         url: siteUrl
     }
-
     // test if bookmarks is null
     if (localStorage.getItem('bookmarks') === null){
         // init array
@@ -31,16 +37,10 @@ function saveBookmark(e) {
         bookmarks.push(bookmark);
         // re-set back to localStorage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    }
-
-
+    }    
+    fetchBookmarks();
     //clear form
     document.getElementById('myForm').reset();
-
-    // re-fetch bookmarks
-    fetchBookmarks();
-    // prevent form from submitting
-    e.preventDefault();
 }
 
 //delete bookmark
@@ -73,40 +73,47 @@ function fetchBookmarks() {
 
         bookmarksResults.innerHTML += 
         '<div class="well">' +
-        '<h3>' + name +
-        ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>' +
-        ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>' +
-
-        '</h3>' +
+        '<h3 id="siteName">' + name + '</h3>' +
+        '<div class="buttons">' +
+        ' <a id="visit" class="btn btn-default" target="_blank" href="'+url+'">Visit</a>' +
+        ' <button id="delete" onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</button>' +
+        '</div>' +
         '</div>';
     }
 }
 
 function validateForm(siteName, siteUrl) {
 
-    var expression = /^((http|https)(:\/\/))?(www.)?([^.]+)(.\w{2,6})(.\w{2,6})?$/i
+    var expression = /^((http|https)(:\/\/))?(www.)?(\w+)(.\w{2,6})(.\w{2,6})?$/i
     var regex = new RegExp(expression);
 
     if (!siteName || !siteUrl) {
         showMessage('Please enter Website Name and URL');
+        return false;
     } else if (!siteUrl.match(regex)){
         showMessage('Please enter valid URL');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function fixUrl(url){
+    expression = /^(http:\/\/|https:\/\/)/i
+    var regex = new RegExp(expression);
+    if (!regex.test(url)){
+        return "https://" + url;
     }
 }
 
 function showMessage(message) {
 
     var check = document.getElementById('errorMessage');
-    
-    if(check) {
-        if (check.innerHTML === message) {
-            console.log(check.innerHTML);
-        } else {
-            const jumbotron = document.querySelector('.jumbotron');
-            jumbotron.removeChild(check);    
-            showMessage(message);
-        }
-    } else {
+
+    const jumbotron = document.querySelector('.jumbotron');
+
+    jumbotron.removeChild(check);    
+
     // create div
     const div = document.createElement('div');
     // add classes
@@ -115,11 +122,9 @@ function showMessage(message) {
     div.id = "errorMessage";
     // add text
     div.appendChild(document.createTextNode(message));
-    // get parent
-    const jumbotron = document.querySelector('.jumbotron');
     // get myForm
     const myForm = document.getElementById('myForm');
     // insert message
     jumbotron.insertBefore(div, myForm);
-    }
+    
 }
